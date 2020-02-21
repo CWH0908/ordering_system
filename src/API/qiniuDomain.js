d699a5f3013d157afff15cbb08fd69bc664f5617
cwh <2515145957@qq.com>
2020-02-21 17:39:41 +0800

    修改七牛云外链为自己的域名

--- src/API/qiniuDomain.js
+++ src/API/qiniuDomain.js
@@ -1,1 +1,4 @@
-export const qiniuDomain = "q3ueuwzr7.bkt.clouddn.com"; //返回七牛的外链地址
+// export const qiniuDomain = "q3ueuwzr7.bkt.clouddn.com"; //返回七牛的外链地址
+
+export const qiniuDomain = "qiniuyun.cwhproject.cn"; //新的域名
+
